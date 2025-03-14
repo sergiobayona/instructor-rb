691ea52baefd9f3bc7b6508aad3da1d0bf2ea1f3
Sergio Bayona <bayona.sergio@gmail.com>
2025-03-14 14:36:29 -0500

    support for structured and tool response

--- lib/instructor/openai/response.rb
+++ lib/instructor/openai/response.rb
@@ -2,69 +2,117 @@
 
 module Instructor
   module OpenAI
-    # The Response class represents the response received from the OpenAI API.
-    # It takes the raw response and provides convenience methods to access the chat completions,
-    # tool calls, function responses, and parsed arguments.
-    class Response
-      # Initializes a new instance of the Response class.
+    module Response
+      # Factory method to create the appropriate response type based on the mode
       #
-      # @param response [Hash] The response received from the OpenAI API.
-      def initialize(response)
-        @response = response
-      end
-
-      # Returns the chat completions from the response.
-      #
-      # @return [Array] An array of chat completions.
-      def chat_completions
-        @response['choices']
-      end
-
-      # Returns the tool calls from the chat completions.
-      #
-      # @return [Hash, nil] The tool calls or nil if not found.
-      def tool_calls
-        chat_completions&.dig(0, 'message', 'tool_calls')
-      end
-
-      # Returns the function responses from the tool calls.
-      #
-      # @return [Array, nil] An array of function responses or nil if not found.
-      def function_responses
-        tool_calls&.map { |tool_call| tool_call['function'] }
-      end
-
-      # Returns the first function response.
-      #
-      # @return [Hash, nil] The first function response or nil if not found.
-      def function_response
-        function_responses&.first
-      end
-
-      # Checks if there is only a single function response.
-      #
-      # @return [Boolean] True if there is only a single function response, false otherwise.
-      def single_response?
-        function_responses&.size == 1
-      end
-
-      # Parses the function response(s) and returns the parsed arguments.
-      #
-      # @return [Array, Hash] The parsed arguments.
-      def parse
-        if single_response?
-          JSON.parse(function_response['arguments'])
+      # @param response [Hash] The response received from the OpenAI API
+      # @return [ToolResponse, StructuredResponse] The appropriate response object
+      def self.create(response)
+        if Instructor::Mode.structured_output?
+          StructuredResponse.new(response)
         else
-          function_responses.map { |res| JSON.parse(res['arguments']) }
+          ToolResponse.new(response)
         end
       end
 
-      # Returns the arguments of the function with the specified name.
-      #
-      # @param function_name [String] The name of the function.
-      # @return [Hash, nil] The arguments of the function or nil if not found.
-      def by_function_name(function_name)
-        function_responses&.find { |res| res['name'] == function_name }&.dig('arguments')
+      # The ToolResponse class represents the response received from the OpenAI API
+      # when using function calling mode. It takes the raw response and provides
+      # convenience methods to access the chat completions, tool calls, function
+      # responses, and parsed arguments.
+      class ToolResponse
+        # Initializes a new instance of the ToolResponse class.
+        #
+        # @param response [Hash] The response received from the OpenAI API.
+        def initialize(response)
+          @response = response
+        end
+
+        # Returns the chat completions from the response.
+        #
+        # @return [Array] An array of chat completions.
+        def chat_completions
+          @response['choices']
+        end
+
+        # Returns the tool calls from the chat completions.
+        #
+        # @return [Hash, nil] The tool calls or nil if not found.
+        def tool_calls
+          chat_completions&.dig(0, 'message', 'tool_calls')
+        end
+
+        # Returns the function responses from the tool calls.
+        #
+        # @return [Array, nil] An array of function responses or nil if not found.
+        def function_responses
+          tool_calls&.map { |tool_call| tool_call['function'] }
+        end
+
+        # Returns the first function response.
+        #
+        # @return [Hash, nil] The first function response or nil if not found.
+        def function_response
+          function_responses&.first
+        end
+
+        # Checks if there is only a single function response.
+        #
+        # @return [Boolean] True if there is only a single function response, false otherwise.
+        def single_response?
+          function_responses&.size == 1
+        end
+
+        # Parses the function response(s) and returns the parsed arguments.
+        #
+        # @return [Array, Hash] The parsed arguments.
+        def parse
+          if single_response?
+            JSON.parse(function_response['arguments'])
+          else
+            function_responses.map { |res| JSON.parse(res['arguments']) }
+          end
+        end
+
+        # Returns the arguments of the function with the specified name.
+        #
+        # @param function_name [String] The name of the function.
+        # @return [Hash, nil] The arguments of the function or nil if not found.
+        def by_function_name(function_name)
+          function_responses&.find { |res| res['name'] == function_name }&.dig('arguments')
+        end
+      end
+
+      # The StructuredResponse class represents the response received from the OpenAI API
+      # when using structured output mode. It takes the raw response and provides
+      # convenience methods to access the chat completions and parse the JSON content.
+      class StructuredResponse
+        # Initializes a new instance of the StructuredResponse class.
+        #
+        # @param response [Hash] The response received from the OpenAI API.
+        def initialize(response)
+          @response = response
+        end
+
+        # Returns the chat completions from the response.
+        #
+        # @return [Array] An array of chat completions.
+        def chat_completions
+          @response['choices']
+        end
+
+        # Returns the content from the first chat completion.
+        #
+        # @return [String, nil] The content or nil if not found.
+        def content
+          chat_completions&.dig(0, 'message', 'content')
+        end
+
+        # Parses the content as JSON and returns the parsed data.
+        #
+        # @return [Hash] The parsed JSON data.
+        def parse
+          JSON.parse(content)
+        end
       end
     end
   end
--- spec/openai/response_spec.rb
+++ spec/openai/response_spec.rb
@@ -3,61 +3,114 @@
 require 'spec_helper'
 
 RSpec.describe Instructor::OpenAI::Response do
-  subject(:response_object) { described_class.new(response) }
+  describe '.create' do
+    let(:response) { { 'choices' => [] } }
 
-  let(:response) do
-    { 'id' => 'chatcmpl-9DEGpBfHqcS17uJtx1vxpRMEb4DtK',
-      'object' => 'chat.completion',
-      'created' => 1_712_940_147,
-      'model' => 'gpt-3.5-turbo-0125',
-      'choices' => [
-        { 'index' => 0,
-          'message' =>
-        { 'role' => 'assistant',
-          'content' => nil,
-          'tool_calls' => [
-            {
-              'id' => 'call_ljjAxRNujNWmDhrlJW2DLprK',
-              'type' => 'function',
-              'function' => { 'name' => 'User', 'arguments' => '{"name": "Jason", "age": 25}' }
-            }
-          ] },
-          'logprobs' => nil,
-          'finish_reason' => 'tool_calls' }
-      ],
-      'usage' => {
-        'prompt_tokens' => 63,
-        'completion_tokens' => 32,
-        'total_tokens' => 95
-      },
-      'system_fingerprint' => 'fp_c2295e73ad' }
+    context 'when in structured output mode' do
+      before { allow(Instructor::Mode).to receive(:structured_output?).and_return(true) }
+
+      it 'returns a StructuredResponse instance' do
+        expect(described_class.create(response)).to be_a(described_class::StructuredResponse)
+      end
+    end
+
+    context 'when in function calling mode' do
+      before { allow(Instructor::Mode).to receive(:structured_output?).and_return(false) }
+
+      it 'returns a ToolResponse instance' do
+        expect(described_class.create(response)).to be_a(described_class::ToolResponse)
+      end
+    end
   end
 
-  it 'returns a chat completion' do
-    expect(response_object.chat_completions).to eq(response['choices'])
+  describe Instructor::OpenAI::Response::ToolResponse do
+    subject(:response_object) { described_class.new(response) }
+
+    let(:response) do
+      { 'id' => 'chatcmpl-9DEGpBfHqcS17uJtx1vxpRMEb4DtK',
+        'object' => 'chat.completion',
+        'created' => 1_712_940_147,
+        'model' => 'gpt-3.5-turbo-0125',
+        'choices' => [
+          { 'index' => 0,
+            'message' =>
+          { 'role' => 'assistant',
+            'content' => nil,
+            'tool_calls' => [
+              {
+                'id' => 'call_ljjAxRNujNWmDhrlJW2DLprK',
+                'type' => 'function',
+                'function' => { 'name' => 'User', 'arguments' => '{"name": "Jason", "age": 25}' }
+              }
+            ] },
+            'logprobs' => nil,
+            'finish_reason' => 'tool_calls' }
+        ],
+        'usage' => {
+          'prompt_tokens' => 63,
+          'completion_tokens' => 32,
+          'total_tokens' => 95
+        },
+        'system_fingerprint' => 'fp_c2295e73ad' }
+    end
+
+    it 'returns a chat completion' do
+      expect(response_object.chat_completions).to eq(response['choices'])
+    end
+
+    it 'returns the tool calls' do
+      expect(response_object.tool_calls).to eq(response['choices'][0]['message']['tool_calls'])
+    end
+
+    it 'returns the function responses' do
+      expect(response_object.function_responses).to eq([response['choices'][0]['message']['tool_calls'][0]['function']])
+    end
+
+    it 'returns the function arguments by function name' do
+      expect(response_object.by_function_name('User')).to eq('{"name": "Jason", "age": 25}')
+    end
+
+    it 'single response' do
+      expect(response_object.single_response?).to eq(true)
+    end
+
+    it 'parses the response' do
+      expect(response_object.parse).to eq('name' => 'Jason', 'age' => 25)
+    end
+
+    it 'returns the first function response' do
+      expect(response_object.function_response).to eq(response['choices'][0]['message']['tool_calls'][0]['function'])
+    end
   end
 
-  it 'returns the tool calls' do
-    expect(response_object.tool_calls).to eq(response['choices'][0]['message']['tool_calls'])
-  end
+  describe Instructor::OpenAI::Response::StructuredResponse do
+    subject(:response_object) { described_class.new(response) }
 
-  it 'returns the function responses' do
-    expect(response_object.function_responses).to eq([response['choices'][0]['message']['tool_calls'][0]['function']])
-  end
+    let(:response) do
+      { 'id' => 'chatcmpl-123',
+        'object' => 'chat.completion',
+        'created' => 1_712_940_147,
+        'model' => 'gpt-3.5-turbo-0125',
+        'choices' => [
+          { 'index' => 0,
+            'message' => {
+              'role' => 'assistant',
+              'content' => '{"name": "Jason", "age": 25}'
+            },
+            'finish_reason' => 'stop' }
+        ] }
+    end
 
-  it 'returns the function arguments by function name' do
-    expect(response_object.by_function_name('User')).to eq('{"name": "Jason", "age": 25}')
-  end
+    it 'returns chat completions' do
+      expect(response_object.chat_completions).to eq(response['choices'])
+    end
 
-  it 'single response' do
-    expect(response_object.single_response?).to eq(true)
-  end
+    it 'returns content' do
+      expect(response_object.content).to eq('{"name": "Jason", "age": 25}')
+    end
 
-  it 'parses the response' do
-    expect(response_object.parse).to eq('name' => 'Jason', 'age' => 25)
-  end
-
-  it 'returns the first function response' do
-    expect(response_object.function_response).to eq(response['choices'][0]['message']['tool_calls'][0]['function'])
+    it 'parses the response' do
+      expect(response_object.parse).to eq('name' => 'Jason', 'age' => 25)
+    end
   end
 end
